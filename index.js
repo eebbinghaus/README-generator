aec5bdb1f1a2af95e26198d721ab4f3f98ee8b8d
Erich Ebbinghaus <eebbinghaus@gmail.com>
2022-11-18 16:08:10 -0700

    added questions to index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is the name of your project?",
+    name: "project",
+  },
+  {
+    type: "input",
+    message: "Please write a short description of your project:",
+    name: "description",
+  },
+  {
+    type: "list",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "none"],
+    name: "license",
+  },
+];
+
+inquirer.prompt(questions).then((response) => {
+  console.log(response);
+});
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
